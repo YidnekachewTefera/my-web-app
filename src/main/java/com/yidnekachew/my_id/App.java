8229df8555084950fc3244d825490fd25c10d7f5
YidnekachewTefera <yomaytec@gmail.com>
2024-03-12 20:13:30 +0300

    my first web app push

---
+++ src/main/java/com/yidnekachew/my_id/App.java
@@ -0,0 +1,13 @@
+package com.yidnekachew.my_id;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/yidnekachew/my_id/AppTest.java
@@ -0,0 +1,20 @@
+package com.yidnekachew.my_id;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
